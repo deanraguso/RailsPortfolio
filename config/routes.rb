f159ba9895459869a832d59dd0d4d63007f73e59
dean <deanraguso@gmail.com>
2021-03-31 19:30:13 +1100

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :folios
+  resources :folios, except: [:show]
+  get 'folio/:id', to: "folios#show", as: "folio_show"
+
   get 'about', to: "pages#about"
   get 'contact', to: "pages#contact"
   resources :blogs
