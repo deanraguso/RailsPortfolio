806e8c6001b1acef85e639e52940bbf1c52a5485
dean <deanraguso@gmail.com>
2021-04-01 12:07:09 +1100

    Integrated default folio images

--- app/models/folio.rb
+++ app/models/folio.rb
@@ -5,4 +5,11 @@ class Folio < ApplicationRecord
         where(subtitle:"Angular")
     end
     scope :rails, -> { where(subtitle:"Rails") }
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.main_image ||= "http://placehold.it/600x400"
+        self.thumb_image ||= "http://placehold.it/350x200"
+    end
 end
